e49e683589322528124463b7905cde5e65a09b9a
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-26 22:21:32 -0300

    Adding Sidebar in the app

--- frontend/application/src/App.tsx
+++ frontend/application/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Sidebar from "./components/sidebar";
 import { GlobalProvider } from "./contexts/global";
 import RoutesWrapper from "./routes";
+import "./translate/";
 
 function App() {
   return (
     <GlobalProvider>
       <Navbar />
+      <Sidebar />
       <RoutesWrapper />
     </GlobalProvider>
   );
