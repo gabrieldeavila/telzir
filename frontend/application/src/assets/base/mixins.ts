02318ac81cb6b47688824b2adeb4327191dc0ecf
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-28 22:36:51 -0300

    Improving styles and locales

--- frontend/application/src/assets/base/mixins.ts
+++ frontend/application/src/assets/base/mixins.ts
@@ -1,4 +1,5 @@
 import { css } from "styled-components";
+import { white } from "./colors";
 
 // mixin with box shadow that can be used in various components
 export const BoxShadow = css`
@@ -19,6 +20,7 @@ export const ContainerSettings = css`
   border-radius: 3px;
 `;
 
+// scrollbar default settings
 export const Scrollbar = css`
   &::-webkit-scrollbar {
     width: 10px;
@@ -28,5 +30,28 @@ export const Scrollbar = css`
   }
   &::-webkit-scrollbar-thumb {
     background: #7f7f83;
+  }
+`;
+
+//default text
+export const Text = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FormSettings = css`
+  ${BoxShadow};
+  font-size: 18px;
+  height: 30px;
+  padding: 1rem;
+  display: block;
+  width: calc(100% - 2rem);
+  border-radius: 5px;
+  background: ${white};
+  border: none;
+
+  &:focus {
+    outline: none;
   }
 `;
