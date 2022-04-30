10624b60fc37362c728279a5ef4089497ccbb137
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-30 12:45:22 -0300

    Adding results component

---
+++ frontend/application/src/components/Simulation/format.ts
@@ -0,0 +1,9 @@
+export const formats = {
+  ddd_from: "string",
+  ddd_to: "string",
+  call_duration: "minutes",
+  choose_plan: "translate",
+  with_telzir: "BRL",
+  without_telzir: "BRL",
+  economy: "percent",
+};
