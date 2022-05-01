2b70c8b71a80fcf077884d97cd35e58a25062621
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-05-01 17:51:00 -0300

    Fixing simulation folder name bug

--- frontend/application/src/redux/effects/Simulate.ts
+++ frontend/application/src/redux/effects/Simulate.ts
@@ -2,7 +2,7 @@ import { GetSimulateResultProps } from "./../interfaces/Simulate";
 import { Dispatch } from "react";
 import { SimulateActionTypes } from "../types/SimulateType";
 import { setSimulateAction } from "../actions/SimulateActions";
-import { SubmitValues } from "./../../components/simulation/interface";
+import { SubmitValues } from "./../../components/Simulation/interface";
 import axios from "axios";
 
 export const setSimulate = (simulateValues: SubmitValues) => {
