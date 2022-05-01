954662fbb9b3d1ea4ca2428ddd88c4438dacaf49
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-04-30 21:38:09 -0300

    Adding Simulation

--- backend/application/src/index.ts
+++ backend/application/src/index.ts
@@ -1,25 +1,97 @@
-const mysql = require("mysql2");
+// require basic scripts
+const { queryData } = require("./scripts/syncDb");
+const { listen } = require("./scripts/listen");
 
-require("dotenv").config();
+const bodyParser = require("body-parser");
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Gabi04avila?",
-  database: "telzir",
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const jsonParser = bodyParser.json();
+
+listen.post("/simulate", jsonParser, async function (req: any, res: any) {
+  let { ddd_from, ddd_to, call_time, choose_plan } = req.body;
+
+  // get the minutes of the plan price and the price
+  let minutes = await getMinutes(choose_plan);
+  let price = await getPrice(ddd_from, ddd_to);
+
+  // compare price with and without the telzir discount
+  let with_telzir = getPlanWithTelzir(call_time, minutes, price);
+  let without_telzir = getPlanWithoutTelzir(call_time, price);
+
+  // get the economy
+  let economy = getEconomy(with_telzir, without_telzir);
+
+  // create the response
+  let response = {
+    with_telzir,
+    without_telzir,
+    economy,
+  };
+
+  // response
+  res.send(response);
 });
 
-con.connect((err: any) => {
-  if (err) {
-    console.log("Error connecting to Db", err);
-    return;
+// query to get the price of the call
+const getPrice = async (ddd_from: string, ddd_to: string) => {
+  let { price } = await queryData(
+    `SELECT price FROM falemais WHERE ddd_from = ? AND ddd_to = ?`,
+    [ddd_from, ddd_to]
+  );
+
+  return price;
+};
+
+// query to get the minutes for free of the plan
+const getMinutes = async (choose_plan: string) => {
+  let { minutes } = await queryData(
+    `SELECT minutes FROM plans WHERE plan = ?`,
+    [choose_plan]
+  );
+
+  return minutes;
+};
+
+// get plan with telzir
+const getPlanWithTelzir = (
+  call_time: number,
+  plan_minutes: number,
+  price: number
+) => {
+  if (!price) return "-";
+
+  // default tariff, if user uses more than the plan minutes
+  const tariff = 10;
+
+  let minutes = call_time - plan_minutes;
+
+  // if the minutes are less than 0, then not all the minutes are used
+  if (minutes < 0) {
+    return 0;
   }
-  console.log("Connection established");
-  console.log(process.env.DB_PWD);
-});
 
-con.end((err: any) => {
-  // The connection is terminated gracefully
-  // Ensures all remaining queries are executed
-  // Then sends a quit packet to the MySQL server.
-});
+  // calculate the price of the minutes with the tariff
+  let price_with_tariff = price + price / tariff;
+  let price_with_minutes = price_with_tariff * minutes;
+
+  return price_with_minutes;
+};
+
+// simple function to get the plan without telzir
+const getPlanWithoutTelzir = (call_time: number, price: number) => {
+  if (!price) return "-";
+
+  return price * call_time;
+};
+
+// get economy that is the difference between the two prices
+const getEconomy = (with_telzir: any, without_telzir: any) => {
+  if (with_telzir === "-" || without_telzir === "-") return "-";
+
+  let percentage = (with_telzir * 100) / without_telzir;
+
+  // calculate the percentege of economy
+  let economy = 100 - percentage;
+
+  return economy;
+};
