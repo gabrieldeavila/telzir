c40743914d271ca636b1f52c069761d487e80838
Gabriel Ávila <gabrielavila04@protonmail.com>
2022-05-01 15:15:36 -0300

    Improving readability

--- backend/application/src/index.ts
+++ backend/application/src/index.ts
@@ -1,5 +1,6 @@
 // require basic scripts
 const { listen } = require("./scripts/listen");
+
 var { simulateAsync } = require("./scripts/simulation");
 
 const bodyParser = require("body-parser");
@@ -10,6 +11,7 @@ const jsonParser = bodyParser.json();
 listen.post("/simulate", jsonParser, async function (req: any, res: any) {
   let { ddd_from, ddd_to, call_time, choose_plan } = req.body;
 
+  // get the simulation
   let response = await simulateAsync(ddd_from, ddd_to, call_time, choose_plan);
 
   // response
